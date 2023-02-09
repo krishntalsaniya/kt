716c83cce3be08d2ec4b558ddaa45d4a3029b387
nulesh <talsaniya.krishn@gmail.com>
2023-02-09 16:36:16 +0530

    sasa
    sasa

---
+++ script.js
@@ -0,0 +1,32 @@
+tippy('#mybtn', {
+    content: "Facebook",
+    placement: 'bottom',
+    animation: 'fade',
+  });
+
+  tippy('#mybtn1', {
+    content: "Whatsapp",
+    placement: 'bottom',
+    animation: 'fade',
+  });
+
+  tippy('#mybtn2', {
+    content: "Instagram",
+    placement: 'bottom',
+    animation: 'fade',
+  });
+
+  tippy('#mybtn3', {
+    content: "Linkedin",
+    placement: 'bottom',
+    animation: 'fade',
+  });
+
+
+
+  tippy('#gototop', {
+    content: "GotoTop",
+    placement: 'bottom-end',
+    animation: 'fade',
+    
+  });
